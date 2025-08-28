3bbfe003b1d30c74e72ec1e597407718306654b1
Sai Koukuntla <sai.koukunt@gmail.com>
2025-08-28 16:25:42 -0400

    implemented infer functions and no obs fit

--- src/fit_infer.jl
+++ src/fit_infer.jl
@@ -1,3 +1,4 @@
+# TODO: add F perturbing and initial correlation check
 function fit_full_model(
     Y::AbstractMatrix{T},
     num_latents::Int,
@@ -20,7 +21,7 @@ function fit_full_model(
     F_perturb_threshold::T = T(1e-5),
     F_noise_sigma::T = T(0.1),
     F_init_max_corr::T = zero(T),
-    F_lr_decay::T = T(0.8),
+    F_lr_decay::T = T(0.999),
     verbose::Bool = true,
 ) where {T<:AbstractFloat}
     """
@@ -42,7 +43,7 @@ function fit_full_model(
         (num_motifs, num_latents, num_latents),
         random_seed,
     )
-    validate_F_separation!(F_init_max_corr)
+    #validate_F_separation!(F_init_max_corr) #TODO: Implement this
     c::Matrix{T} = init_matrix(
         InitDistribution.Normal(),
         (num_motifs, num_timepoints - 1),
@@ -90,20 +91,6 @@ function fit_full_model(
             frobenius_coeff = D_frobenius_coeff,
         )
 
-        update_F!(
-            F,
-            gradient_sum,
-            temp_gradient,
-            x_hat_next,
-            update_F_residuals,
-            X,
-            c,
-            F_lr;
-            normalize_F = F_normalize_matrix,
-            normalize_gradient = F_normalize_gradient,
-        )
-        F_lr *= F_lr_decay
-
         data_recon_err = calculate_data_recon_error!(data_prediction, Y, D, X)
         latent_recon_err[i] = calculate_latent_recon_error!(x_hat_next, F, X, c)
 
@@ -118,11 +105,147 @@ function fit_full_model(
     return D, F, X, c, latent_recon_err
 end
 
-function fit_no_obs_model() end
+function fit_no_obs_model(
+    X::AbstractMatrix{T},
+    num_motif::Int;
+    random_seed::Int = 0,
+    max_iter::Int = 3000,
+    c_l1_coeff::T = zero(T),
+    c_l1_coeff_decay::T = T(1),
+    c_smooth_coeff::T = zero(T),
+    c_fista_tol::T = T(1e-8),
+    c_fista_max_iter::Int = 1000,
+    F_normalize_matrix::Bool = true,
+    F_normalize_gradient::Bool = false,
+    F_perturb_threshold::T = T(1e-5),
+    F_noise_sigma::T = T(0.1),
+    F_init_max_corr::T = zero(T),
+    F_lr_decay::T = T(0.999),
+    verbose::Bool = true,
+) where {T<:AbstractFloat}
+    num_latents::Int = size(X, 1)
+    num_timepoints::Int = size(X, 2)
 
-function infer_no_obs_model() end
+    # Initialize model parameters and state
+    F::Array{T,3} = init_matrix(
+        InitDistribution.Normal(),
+        (num_motifs, num_latents, num_latents),
+        random_seed,
+    )
+    #validate_F_separation!(F_init_max_corr) #TODO: Implement this
+    c::Matrix{T} = init_matrix(
+        InitDistribution.Normal(),
+        (num_motifs, num_timepoints - 1),
+        random_seed,
+    )
 
-function infer_full_model() end
+    F_lr::T = F_lr_init
+    i::Int = 1
+    latent_recon_err::Vector{T} = zeros(T, num_timepoints)
+
+    FX_prod::Matrix{T} = Matrix{T}(undef, num_latents, num_motifs) # for update_c!
+    gradient_sum::Array{T,3} = similar(F)                          # for update f! 
+    temp_gradient::Matrix{T} = Matrix{T}(undef, num_latents, num_latents)
+    x_hat_next::Vector{T} = Vector{T}(undef, num_latents)
+    update_F_residuals::Vector{T} = Vector{T}(undef, num_latents)
+
+    while (latent_recon_err > recon_threshold) && (i <= max_iter)
+        update_X!(X, D, Y, lambda_l1 = x_l1_coeff)
+
+        c_l1_coeff *= c_l1_coeff_decay
+        update_c!(
+            c,
+            FX_prod,
+            X,
+            F,
+            smooth_coeff = c_smooth_coeff,
+            l1_coeff = c_l1_coeff;
+            tol = c_fista_tol,
+            max_iter = c_fista_max_iter,
+            warm_start = i > 1 ? c_fista_warm_start : false,
+        )
+
+        update_F!(
+            F,
+            gradient_sum,
+            temp_gradient,
+            x_hat_next,
+            update_F_residuals,
+            X,
+            c,
+            F_lr;
+            normalize_F = F_normalize_matrix,
+            normalize_gradient = F_normalize_gradient,
+        )
+        F_lr *= F_lr_decay
+
+        latent_recon_err[i] = calculate_latent_recon_error!(x_hat_next, F, X, c)
+
+        if verbose
+            println(
+                "Iter $(i): Data Rec. Error: $(data_recon_err), Latent Rec. Error: $(latent_recon_err[i]) ",
+            )
+        end
+        i += 1
+    end
+
+    return F, c, latent_recon_err
+end
+
+function infer_no_obs_state(
+    F::AbstractArray{T,3},
+    X::AbstractMatrix{T};
+    c_l1_coeff::T = zero(T),
+    c_smooth_coeff::T = zero(T),
+    c_fista_tol::T = T(1e-8),
+    c_fista_max_iter::Int = 1000,
+) where {T<:AbstractFloat}
+    c::Matrix{T} = init_matrix(
+        InitDistribution.Normal(),
+        (num_motifs, num_timepoints - 1),
+        random_seed,
+    )
+
+    FX_prod::Matrix{T} = Matrix{T}(undef, num_latents, num_motifs)
+    update_c!(
+        c,
+        FX_prod,
+        X,
+        F,
+        smooth_coeff = c_smooth_coeff,
+        l1_coeff = c_l1_coeff;
+        tol = c_fista_tol,
+        max_iter = c_fista_max_iter,
+        warm_start = i > 1 ? c_fista_warm_start : false,
+    )
+
+    return c
+end
+
+function infer_full_state(
+    D::AbstractMatrix{T},
+    F::AbstractArray{T,3},
+    Y::AbstractMatrix{T};
+    x_l1_coeff::T = zero(T),
+    c_l1_coeff::T = zero(T),
+    c_smooth_coeff::T = zero(T),
+    c_fista_tol::T = T(1e-8),
+    c_fista_max_iter::Int = 1000,
+) where {T<:AbstractFloat}
+    X::Matrix{T} = Matrix{T}(undef, num_latents, num_timepoints)
+    update_X!(X, D, Y, lambda_l1 = x_l1_coeff)
+
+    c = infer_no_obs_state(
+        F,
+        X;
+        c_l1_coeff = c_l1_coeff,
+        c_smooth_coeff = c_smooth_coeff,
+        c_fista_tol = c_fista_tol,
+        c_fista_max_iter = c_fista_max_iter,
+    )
+
+    return X, c
+end
 
 function calculate_data_recon_error!(
     prediction::AbstractMatrix{T},
